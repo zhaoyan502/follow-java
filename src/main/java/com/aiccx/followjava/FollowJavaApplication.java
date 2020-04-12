58ef34bf95f9e7c91769e07a4b3a5f0edc87b206
zhaoyan <zhaoyan502@126.com>
2020-04-12 20:48:03 +0800

    init.

---
+++ src/main/java/com/aiccx/followjava/FollowJavaApplication.java
@@ -0,0 +1,13 @@
+package com.aiccx.followjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiccx/followjava/FollowJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiccx.followjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
